8ad8fd6b494c65eb50a295b8e92495ffddbf11b2
alec <alecrondel@hotmail.com>
2024-02-26 15:41:31 -0300

    options api vs composition api

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,84 @@
+<template>
+  <div class="home">
+    
+    <div>
+      <button @click="decreaseCounter" class="btn">-</button>
+      <span class="counter">{{ counter }}</span>
+      <button @click="increaseCounter" class="btn">+</button>
+    </div>
+
+  </div>
+</template>
+
+
+<!-- SCRIPT SETUP PATTERN -->
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+  import { ref } from 'vue';
+
+  const counter = ref(0)
+
+  const increaseCounter = () => {
+    counter.value++
+  }
+
+  const decreaseCounter = () => {
+    counter.value--
+  }
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<!-- SETUP FUNCTION PATTERN -->
+<!-- <script>
+
+  import { ref } from 'vue';
+
+  export default {
+    setup() {
+      const counter = ref(0)
+
+      const increaseCounter = () => {
+        counter.value++
+      }
+
+      const decreaseCounter = () => {
+        counter.value--
+      }
+
+      return {
+        counter,
+        increaseCounter,
+        decreaseCounter
+      }
+    }
+  }
+</script> -->
+
+<!-- OPTIONS API PATTERN -->
+<!-- <script>
+  export default {
+    data() {
+      return {
+        counter: 0
+      }
+    },
+    methods: {
+      increseCounter() {
+        this.counter++
+      },
+      decreaseCounter() {
+        this.counter--
+      }
+    }
+  }
+</script> -->
+
+<style>
+  .home {
+    text-align: center;
+    padding: 20px;
+  }
+
+  .btn, .counter {
+    font-size: 40px;
+    margin: 10px;
+  }
+</style>
