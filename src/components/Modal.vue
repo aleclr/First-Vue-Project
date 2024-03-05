dfe82fba5ed9f6fdcbaddf6b98c2fbc4212da53b
alec <alecrondel@hotmail.com>
2024-03-05 10:54:08 -0300

    importing components

---
+++ src/components/Modal.vue
@@ -0,0 +1,23 @@
+<template>
+  <Teleport to=".modals-container">
+      <div class="modal">
+        <h1>This is a modal</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur perferendis officiis nemo cupiditate commodi natus ipsum at, est accusamus totam repellat illo officia aperiam, accusantium excepturi harum numquam nostrum asperiores!</p>
+        <button>Hide modal</button>
+      </div>
+    </Teleport>
+</template>
+
+<style>
+
+  .modal {
+    background-color: beige;
+    padding: 10px;
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+  }
+
+</style>
--- src/views/ModalsView.vue
+++ src/views/ModalsView.vue
@@ -3,13 +3,7 @@
   <div class="modals">
     <h1>Modals</h1>
     <button @click="showModal = true">Show modal</button>
-    <Teleport to=".modals-container">
-      <div class="modal" v-if="showModal">
-        <h1>This is a modal</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur perferendis officiis nemo cupiditate commodi natus ipsum at, est accusamus totam repellat illo officia aperiam, accusantium excepturi harum numquam nostrum asperiores!</p>
-        <button @click="showModal = false">Hide modal</button>
-      </div>
-    </Teleport>
+    <Modal v-if="showModal"></Modal>
   </div>
 
 </template>
@@ -17,6 +11,7 @@
 <script setup>
 
 import { ref } from 'vue';
+import Modal from '@/components/Modal.vue';
 
 // MODALS
 const showModal = ref(false);
@@ -24,17 +19,3 @@ const showModal = ref(false);
 
 
 </script>
-
-<style>
-
-  .modal {
-    background-color: beige;
-    padding: 10px;
-    position: absolute;
-    left: 0;
-    top: 0;
-    width: 100%;
-    height: 100%;
-  }
-
-</style>
